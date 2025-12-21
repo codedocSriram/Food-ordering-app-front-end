5f57428748f43916cffbc55e94f44e9f01f138b6
Sriram Annadurai <mailsriram98@gmail.com>
2025-12-21 11:50:18 +0530

    Added Cart Modal functionalities

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
-import Button from "./HeaderCartButton";
-const Header = () => {
+import HeaderCartButton from "./HeaderCartButton";
+const Header = props => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>TastyMeals</h1>
-        <Button />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="stock food image" />
